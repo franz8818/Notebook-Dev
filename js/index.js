1b396e8d2f4c100b11bda63baac38ec2f0140ccc
Franz Seidel <franz8818@gmail.com>
2023-01-26 23:00:05 +0000

    repaso js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-alert("Hola Franz")
+//EJEMPLO if/else
+// PARA PARSEAR LOS DATOS
+// USAMOS parseInt
+// CAMBIA DE UN STRING A UN DATO NUMERICO
+
+let edad = parseInt(prompt("ingresa tu edad"))
+if (edad === 18) {
+    alert("welcome to my world")
+} else {
+    alert("access denied")
+}
