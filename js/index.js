1bfc7e950577ffdacc1fffc5473d6a4425187272
Franz Seidel <franz8818@gmail.com>
2023-02-06 21:03:21 +0000

    llamar a una funcion

--- js/index.js
+++ js/index.js
@@ -18,8 +18,18 @@ const persona = {
 //CONSULTAR DATOS DE UN OBJETO
 console.log(persona.apellido);
 
-
-//DECLARACION DE UN array
+//DECLARACION DE UN array 
+//INDICE O POSICION 0         1.      2
 const frutas = ["Manzana", "Pera", "Mango"]
+//LARGO=LENGTH      1         2.      3
+
 //CONSULTAR UN VALOR DE UN array
 console.log(frutas[1]);
+
+//FUNCIONES
+function saludar() { //DECLARACION DE UNA FUNCION
+    //BLOQUE DE CODIGO A EJECUTAR
+    console.log("Hola vengo de la funcion");
+}
+//LLAMAR UNA FUNCION O "INVOCAR"
+saludar() //LLAMANDO A LA FUNCION
