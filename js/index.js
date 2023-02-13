6cd2fe0f059a247b8d44f67fac97d4f6b46e0a71
Franz Seidel <franz8818@gmail.com>
2023-02-13 18:02:03 +0000

    array y objetos

--- js/index.js
+++ js/index.js
@@ -3,33 +3,51 @@
 // USAMOS parseInt
 // CAMBIA DE UN STRING A UN DATO NUMERICO
 
-let edad = parseInt(prompt("ingresa tu edad"))
-if (edad === 18) {
-    alert("welcome to my world")
-} else {
-    alert("access denied")
-}
+// let edad = parseInt(prompt("ingresa tu edad"))
+// if (edad === 18) {
+//     alert("welcome to my world")
+// } else {
+//     alert("access denied")
+// }
 
 //DECLARACION DE UN objeto literal
 const persona = {
     nombre: "Camilo",
-    apellido: "Sanchez"
+    apellido: "Sanchez",
+    edad: 35,
+    is_activo: true,
+    saludar: function() {
+        console.log("Holas a todos")
+    }
 }
 //CONSULTAR DATOS DE UN OBJETO
 console.log(persona.apellido);
 
-//DECLARACION DE UN array 
+//ARRAY
 //INDICE O POSICION 0         1.      2
 const frutas = ["Manzana", "Pera", "Mango"]
 //LARGO=LENGTH      1         2.      3
 
 //CONSULTAR UN VALOR DE UN array
 console.log(frutas[1]);
+deportes.push("baloncesto") //INGRESAR NUEVO DATO AL FINAL DE LA ARRAY
+deportes.pop() //ELIMINAR UN DATO AL FINAL DE LA ARRAY
 
-//FUNCIONES
+//FUNCIONES - FUNCION DE DECLARACION
 function saludar() { //DECLARACION DE UNA FUNCION
     //BLOQUE DE CODIGO A EJECUTAR
     console.log("Hola vengo de la funcion");
 }
 //LLAMAR UNA FUNCION O "INVOCAR"
 saludar() //LLAMANDO A LA FUNCION
+
+let busqueda = prompt("Ingrese su busqueda")
+
+let pos = frutas.indexOf("busqueda");
+if (pos >= 0) {
+    alert("si existe")
+} else {
+    alert("No se encontraron")
+}
+
+//ESTRUCTURAS DE CONTROL
