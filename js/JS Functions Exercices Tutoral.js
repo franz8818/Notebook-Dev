67dea5e8b03882ed5a472fd627b35bdc0d28b8fe
Franz Seidel <franz8818@gmail.com>
2023-02-16 14:48:19 +0000

    Empezamos con las funciones

---
+++ js/JS Functions Exercices Tutoral.js
@@ -0,0 +1,2 @@
+//EXERCISE 1 - Imprimir en la consola "Hello World"
+console.log("Hello World");
--- js/Javascript Beginner Tutorial.js
+++ js/Javascript Beginner Tutorial.js
@@ -1,34 +1,34 @@
-//EXERCISE 1
+//EXERCISE 1 - Hello World
 console.log("Hello World");
 
-//EXERCISE 2
+//EXERCISE 2 - Print Variables to the Console
 var mySuperVariable = 'hello';
 console.log(mySuperVariable);
 
 var color = "red"
 console.log(color)
 
-//EXERCISE 3
+//EXERCISE 3 - Multiply Two Values
 const variablesAreCool = 2345 * 7323
 console.log(variablesAreCool);
 
-//EXERCISE 4
+//EXERCISE 4 - User Inputted Variables
 var age = parseInt(prompt('What is your age?'));
 
 let nuevaEdad = age + 10;
 console.log(nuevaEdad);
 
-//EXERCISE 5
+//EXERCISE 5 - Constants
 const VERSION = '0.9';
 console.log(VERSION);
 
-//EXERCISE 6 - NO FUNCIONO - CONSULTAR
+//EXERCISE 6 - String Concatenation - NO FUNCIONO - CONSULTAR
 let myVar1 = "Hello";
 let myVar2 = "World";
 
 console.log(myVar1 + " " + myVar2);
 
-//EXERCISE 7
+//EXERCISE 7 - Creating Basic HTML Code
 const a = '</title>';
 const b = '</html>';
 const c = '<head>';
@@ -42,3 +42,5 @@ const h = '<body>';
 let htmlDocument = e + c + g + a + f + h + d + b
 
 console.log(htmlDocument);
+
+//EXERCISE 8 - Calling Your First Function
