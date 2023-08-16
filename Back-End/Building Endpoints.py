12f54e22134c4310c4a57e8a17174ed8c0087a1d
Franz Seidel <109550161+franz8818@users.noreply.github.com>
2023-08-16 21:06:42 +0000

    endpoints

---
+++ Back-End/Building Endpoints.py
@@ -0,0 +1,17 @@
+#PARA REALIZAR LA CONSTRUCCION DE UN SERVIDOR
+#CONCEPTO 1 - METODOS
+
+"""
+>Get -> Obtener datos 
+>Put -> Actualizar esos datos
+>Post -> Crear esos datos
+>Delete -> Borrar esos datos
+"""
+
+#CONCEPTO 2 - CODIGOS DE RESPUESTA(DEVOLVER) - RUTAS CREADAS
+
+"""Van desde el 100 al 599
+Tienen un rango para decir algo en especifico, Ejemplo: error al recibir un datos.
+"""
+
+
