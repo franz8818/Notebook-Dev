57bdff3b100c389b724df3934617efbcb869268e
Franz Seidel <109550161+franz8818@users.noreply.github.com>
2023-08-16 22:51:41 +0000

    SQL

---
+++ Back-End/Bases de datos.py
@@ -0,0 +1,18 @@
+#BASES DE DATOS
+#Conjunto de datos pertenecientes a un mismo contexto y almacenados sistemáticamente para su posterior uso.
+#Ejemplo -> Biblioteca, jeroglíficos, cuadernos, blog de notas, excel.
+
+#SQL -> Structured Query Language
+#Relacional -> Basada en tablas
+#Mantienen una estructura definida
+#Aplicaciones para datos consistentes -> Ejemplo -> Datos bancarios
+#Herramientas Relacionales -> MySQL, Oracle, Microsoft Access
+
+#NoSQL -> Structured Query Language
+#No Relacional -> Basada en documentos
+#Estructura dinámica
+#Aplicaciones que requieren grandes cantidades de datos a consultar -> Ejemplo -> Datos de las Redes Sociales
+#Herramientas No Relacionales -> MongoDB, AzureDB, DynamoDB
+
+
+
